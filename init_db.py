4e38833a03a455a0f79ccae1429120d0472a49a5
Jazem Siddiqui <jazemf.s@gmail.com>
2022-02-21 23:28:20 -0800

    reset db

--- init_db.py
+++ init_db.py
@@ -8,11 +8,5 @@ connection = sqlite3.connect('database.db')
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
-cur = connection.cursor()
-
-cur.execute("INSERT INTO tasks_3035705646 (title, is_completed) VALUES (?, ?)",
-            ('Test_name', False)
-            )
-
 connection.commit()
 connection.close()
